98a8e979d52fbbb9da4c964a146eed9d9ad13480
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-13 13:11:21 +0000

    New drop scripts to remove existing aliases
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ drop_aliases_prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export ES_URL="`cat ES_URL.prod.secret`"
+for idx in bitergia-github_cloudfoundry_180322_enriched_191002 bitergia-github_oci_180322_enriched_190725 bitergia-github_symphonyoss_180322_enriched_200930
+do
+  curl -s -XDELETE "${ES_URL}/${idx}/_alias/sds-*-github-issue"
+done
+for idx in bitergia-github-prs_symphonyoss_190214b_enriched_200930
+do
+  curl -s -XDELETE "${ES_URL}/${idx}/_alias/sds-*-github-issue"
+  curl -s -XDELETE "${ES_URL}/${idx}/_alias/sds-*-github-pull_request"
+done
+for idx in bitergia-git_cloudfoundry_181221_enriched_191007 bitergia-git_oci_181221_enriched_191007 bitergia-git_onap_191112_enriched_191112 bitergia-git_opendaylight_181221_enriched_200521 bitergia-git_opnfv_181220_enriched_191007 bitergia-git_symphonyoss_200604_enriched_200930 bitergia-git_yoctoproject_200109_enriched_200109
+do
+  curl -s -XDELETE "${ES_URL}/${idx}/_alias/sds-*-git"
+done
