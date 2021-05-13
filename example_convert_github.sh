d93877da9bcb7228a01bd9afcd3aedbdd56d8011
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-13 09:25:34 +0000

    remove unneeded scripts
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- example_convert_github.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-NCPUS=10 ES_URL="`cat ES_URL.prod.secret`" ./es-convert-to-dads github bitergia-github_symphonyoss_180322_enriched_200930 bitergia-github_symphonyoss_180322_enriched_200930-converted
