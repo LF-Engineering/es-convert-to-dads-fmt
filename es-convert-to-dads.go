39003e6b051815ac6bf82075460b2ef5168ba754
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-13 04:24:25 +0000

    WIP 2
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es-convert-to-dads.go
+++ es-convert-to-dads.go
@@ -8,7 +8,11 @@ import (
 )
 
 var (
-	gESURL string
+	gESURL   string
+	gDSTypes = map[string]struct{}{
+		"git":    {},
+		"github": {},
+	}
 )
 
 func fatalOnError(err error) {
@@ -25,6 +29,26 @@ func fatalf(f string, a ...interface{}) {
 	fatalOnError(fmt.Errorf(f, a...))
 }
 
+func convertGitHub(idxFrom, idxTo string) (err error) {
+	return nil
+}
+
+func convertGit(idxFrom, idxTo string) (err error) {
+	return fmt.Errorf("git is not implemented yet")
+}
+
+func convert(dsType, idxFrom, idxTo string) (err error) {
+	switch dsType {
+	case "git":
+		err = convertGit(idxFrom, idxTo)
+	case "github":
+		err = convertGitHub(idxFrom, idxTo)
+	default:
+		err = fmt.Errorf("%s support not implemented", dsType)
+	}
+	return
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		fatalf("ES_URL=... %s: ds-type from-index to-index\n", os.Args[0])
@@ -35,4 +59,11 @@ func main() {
 		fatalf("%s: you need to set ES_URL environment variable\n", os.Args[0])
 		return
 	}
+	dsType := os.Args[1]
+	_, ok := gDSTypes[dsType]
+	if !ok {
+		fatalf("%s: %s is not a know ds-type, allowed are: %+v\n", os.Args[0], dsType, gDSTypes)
+		return
+	}
+	fatalOnError(convert(dsType, os.Args[2], os.Args[3]))
 }
