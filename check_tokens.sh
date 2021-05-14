4c03837b07bdfb778d52a5eb8058a9240fccb09b
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-14 12:39:52 +0000

    Tokens ...
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- check_tokens.sh
+++ check_tokens.sh
@@ -6,7 +6,9 @@ do
     f="$(echo "$f" | tr -d '\n' | tr -d '\r')"
     points="`curl -sH "Authorization: token $f" https://api.github.com/users/codertocat -I | grep --color=never x-ratelimit-limit`"
     remains="`curl -sH "Authorization: token $f" https://api.github.com/users/codertocat -I | grep --color=never x-ratelimit-remaining`"
+    reset="`curl -sH "Authorization: token $f" https://api.github.com/users/codertocat -I | grep --color=never x-ratelimit-reset`"
     points="$(echo "$points" | tr -d '\n' | tr -d '\r')"
     remains="$(echo "$remains" | tr -d '\n' | tr -d '\r')"
-    echo "token $f $points $remains"
+    reset="$(echo "$reset" | tr -d '\n' | tr -d '\r')"
+    echo "token $f $points $remains $reset"
 done
