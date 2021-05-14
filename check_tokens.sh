f0f888ec83c7394b5f75eb18a80c3e14e93928d3
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-14 12:38:31 +0000

    Check tokens script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ check_tokens.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+tokens="`cat ../sync-data-sources/helm-charts/sds-helm/sds-helm/secrets/GITHUB_OAUTH.secret`"
+tokens="${tokens//,/ }"
+for f in $tokens
+do
+    f="$(echo "$f" | tr -d '\n' | tr -d '\r')"
+    points="`curl -sH "Authorization: token $f" https://api.github.com/users/codertocat -I | grep --color=never x-ratelimit-limit`"
+    remains="`curl -sH "Authorization: token $f" https://api.github.com/users/codertocat -I | grep --color=never x-ratelimit-remaining`"
+    points="$(echo "$points" | tr -d '\n' | tr -d '\r')"
+    remains="$(echo "$remains" | tr -d '\n' | tr -d '\r')"
+    echo "token $f $points $remains"
+done
