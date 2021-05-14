acc4e660569f8f805e0781e5cc6aec8ad09e78b7
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-14 10:29:20 +0000

    Updates to script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- delete_all_but_historical_prod.sh
+++ delete_all_but_historical_prod.sh
@@ -3,12 +3,12 @@
 # DRP - drop non-historical endpoints from converted indices
 export ES_URL="`cat ES_URL.prod.secret`"
 #curl -s -H 'Content-Type: application/json' -XPOST "${ES_URL}/sds-gql-gql-git-converted/_delete_by_query" -d'{"query":{"bool":{"must_not":{"term":{"origin":"https://github.com/graphql/tsc"}}}}}' | jq '.deleted'
-#curl -s -H 'Content-Type: application/json' -XPOST "https://elastic:8khOn4tiemn5mcYZkIa3w1OB@20e7bc15b6584b6f878c9b6d75ab8fe9.us-west-1.aws.found.io:9243/sds-hyperledger-hyperledger-all-git-converted/_search?size=10000" -d'{"query":{"bool":{"should":[{"term":{"origin":"https://github.com/hyperledger/aries-ams-sqlite"}},{"term":{"origin":"https://github.com/hyperledger-twgc/grpc-gm"}}]}}}' | jq '.hits.hits[]._source.origin' | sort | uniq
-#curl -s -H 'Content-Type: application/json' -XPOST "https://elastic:8khOn4tiemn5mcYZkIa3w1OB@20e7bc15b6584b6f878c9b6d75ab8fe9.us-west-1.aws.found.io:9243/sds-hyperledger-hyperledger-all-git-converted/_search?size=10000" -d'{"query":{"bool":{"must_not":[{"term":{"origin":"https://github.com/hyperledger/aries-ams-sqlite"}},{"term":{"origin":"https://github.com/hyperledger-twgc/grpc-gm"}}]}}}' | jq '.hits.hits[]._source.origin' | sort | uniq
+#curl -s -H 'Content-Type: application/json' -XPOST "${ES_URL}/sds-hyperledger-hyperledger-all-git-converted/_search?size=10000" -d'{"query":{"bool":{"should":[{"term":{"origin":"https://github.com/hyperledger/aries-ams-sqlite"}},{"term":{"origin":"https://github.com/hyperledger-twgc/grpc-gm"}}]}}}' | jq '.hits.hits[]._source.origin' | sort | uniq
+#curl -s -H 'Content-Type: application/json' -XPOST "${ES_URL}/sds-hyperledger-hyperledger-all-git-converted/_search?size=10000" -d'{"query":{"bool":{"must_not":[{"term":{"origin":"https://github.com/hyperledger/aries-ams-sqlite"}},{"term":{"origin":"https://github.com/hyperledger-twgc/grpc-gm"}}]}}}' | jq '.hits.hits[]._source.origin' | sort | uniq
 # sds-hyperledger-hyperledger-all-git - two origins: https://github.com/hyperledger/aries-ams-sqlite, https://github.com/hyperledger-twgc/grpc-gm
 if [ ! -z "$DROP" ]
 then
-  curl -s -H 'Content-Type: application/json' -XPOST "https://elastic:8khOn4tiemn5mcYZkIa3w1OB@20e7bc15b6584b6f878c9b6d75ab8fe9.us-west-1.aws.found.io:9243/sds-hyperledger-hyperledger-all-git-converted/_delete_by_query" -d'{"query":{"bool":{"must_not":[{"term":{"origin":"https://github.com/hyperledger/aries-ams-sqlite"}},{"term":{"origin":"https://github.com/hyperledger-twgc/grpc-gm"}}]}}}' | jq '.deleted'
+  curl -s -H 'Content-Type: application/json' -XPOST "${ES_URL}/_delete_by_query" -d'{"query":{"bool":{"must_not":[{"term":{"origin":"https://github.com/hyperledger/aries-ams-sqlite"}},{"term":{"origin":"https://github.com/hyperledger-twgc/grpc-gm"}}]}}}' | jq '.deleted'
 fi
 declare -A data
 data["sds-gql-gql-git-converted"]="https://github.com/graphql/tsc"
