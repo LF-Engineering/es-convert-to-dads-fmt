505f38e42269b91aa83ef6a3ef7ea22a43890fd6
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-14 07:45:28 +0000

    Finder script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- search_historical.sh
+++ search_historical.sh
@@ -14,11 +14,16 @@ export ES_URL="`cat ES_URL.prod.secret`"
 where="${1}"
 what="${2}"
 where="${where//\//-}"
-echo "${where}:${what}"
 curl -s -XPOST -H 'Content-Type: application/json' "${ES_URL}/_sql?format=txt" -d"{\"query\":\"select origin, count(*) as cnt from \\\"sds-${where}-git*,-*-raw\\\" where origin like '%${what}%' group by origin order by cnt desc\"}"
 indices=$(curl -s -H 'Content-Type: application/json' "${ES_URL}/sds-${where}-git*,-*-raw/_search?size=10000" -d"{\"query\":{\"term\":{\"origin\":\"https://github.com/${what}\"}}}" | jq -rS '.hits.hits[]._index' | sort | uniq)
+if [ ! -z "$indices" ]
+then
+  echo ''
+  echo "${where}:${what} exact matches"
+fi
 for idx in $indices
 do
   echo "index: $idx"
   curl -s -XPOST -H 'Content-Type: application/json' "${ES_URL}/_sql?format=txt" -d"{\"query\":\"select origin, count(*) as cnt from \\\"${idx}\\\" where origin = 'https://github.com/${what}' group by origin order by cnt desc\"}"
+  echo ''
 done
