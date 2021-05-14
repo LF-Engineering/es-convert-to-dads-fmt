1be2b5456b457a734e36ec3c873764399b09550c
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-14 11:45:20 +0000

    One more index
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- prod_restore_historical.sh
+++ prod_restore_historical.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DROP=1 - to drop reindexed index
 export ES_URL="`cat ES_URL.prod.secret`"
-for idx in sds-gql-gql-github-repository sds-hyperledger-aries-github-issue sds-hyperledger-aries-github-repository sds-hyperledger-hyperledger-twgc-github-issue sds-hyperledger-hyperledger-twgc-github-repository sds-jdf-toip-github-repository sds-open-mainframe-project-zowe-github-repository sds-tarscloud-github-issue sds-tarscloud-github-repository
+for idx in sds-hyperledger-hyperledger-all-git sds-gql-gql-github-repository sds-hyperledger-aries-github-issue sds-hyperledger-aries-github-repository sds-hyperledger-hyperledger-twgc-github-issue sds-hyperledger-hyperledger-twgc-github-repository sds-jdf-toip-github-repository sds-open-mainframe-project-zowe-github-repository sds-tarscloud-github-issue sds-tarscloud-github-repository
 do
   echo "reindex ${idx}"
   curl -s -XPOST -H 'Content-Type: application/json' "${ES_URL}/_reindex" -d "{\"source\":{\"index\":\"${idx}-converted\"},\"dest\":{\"index\":\"${idx}\"}}"
