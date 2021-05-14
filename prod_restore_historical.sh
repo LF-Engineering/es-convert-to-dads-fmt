ec927760cfc8005ce07887bbd31565ce0a7c9f31
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-05-14 10:50:34 +0000

    Reindex/drop
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- prod_restore_historical.sh
+++ prod_restore_historical.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# DROP=1 - to drop reindexed index
 export ES_URL="`cat ES_URL.prod.secret`"
 for idx in sds-gql-gql-github-repository sds-hyperledger-aries-github-issue sds-hyperledger-aries-github-repository sds-hyperledger-hyperledger-twgc-github-issue sds-hyperledger-hyperledger-twgc-github-repository sds-jdf-toip-github-repository sds-open-mainframe-project-zowe-github-repository sds-tarscloud-github-issue sds-tarscloud-github-repository
 do
   echo "reindex ${idx}"
   curl -s -XPOST -H 'Content-Type: application/json' "${ES_URL}/_reindex" -d "{\"source\":{\"index\":\"${idx}-converted\"},\"dest\":{\"index\":\"${idx}\"}}"
+  echo ''
+  if [ ! -z "$DROP" ]
+  then
+    curl -s -XDELETE "${ES_URL}/${idx}-converted"
+    echo ''
+  fi
 done
